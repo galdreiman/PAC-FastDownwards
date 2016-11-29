4923ce4353aa23285ee22c0a5584bfc30c76b754
galdreiman <gal.dreiman@gmail.com>
2016-11-29 16:36:00 +0200

    update run_search

--- run_search.sh
+++ run_search.sh
@@ -2,15 +2,15 @@
 
 
 
-echo "/home/shahar/downward/benchmarks/$1/$2"
+echo "/home/gal-d/downward/benchmarks/$1/$2"
 echo "start"
 
-cd /home/shahar/downward/src/
+cd /home/gal-d/downward/src/
 translate/translate.py /home/shahar/downward/benchmarks/$1/$2
 preprocess/preprocess < output.sas
 search/downward --search "astar(lmcut())" < output
 
-echo "/home/gal/downward/benchmarks/$1/$2"
+echo "/home/gal-d/downward/benchmarks/$1/$2"
 echo "end" 
 
 exit
